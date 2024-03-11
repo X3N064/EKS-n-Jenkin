23ab3af8c20088cb80af89d31e653a85a5bbbb50
Hyun Sim <kitoko148@gmail.com>
2024-03-11 01:17:38 +0000

    done???

--- tfs-eks/provider.tf
+++ tfs-eks/provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
-  region                   = "us-east-1"
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "terra"
+  region = "us-east-1"
 }
